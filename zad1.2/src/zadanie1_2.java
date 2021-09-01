09d91631bd9938e9af66671bb0d15ac6f1b87309
Sergey Korneev <1337zone@mail.ru>
2021-09-01 16:49:06 +0300

    Cod second ex

--- zad1.2/src/zadanie1_2.java
+++ zad1.2/src/zadanie1_2.java
@@ -1,3 +1,10 @@
 public class zadanie1_2 {
 
-}
+        public static void zadanie1_2 (String[] args) {
+            double regularBonus = 0.3;
+            double specialBonus = 0.6;
+            double totalBonus = regularBonus + specialBonus;
+            System.out.println(totalBonus);
+        }
+    }
+
